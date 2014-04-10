7d4867fbb6553dd9e57bbfe2c4e1394dfa46bac1
Peter Keogh <keoghpe@tcd.ie>
2014-04-11 03:01:06 +0800

    Basic setup for e2e testing

--- Test/e2e/scenarios.js
+++ Test/e2e/scenarios.js
@@ -1,51 +1,20 @@
-'use strict';
-
-/* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
-
-
-
-
-
-
-describe('my app', function() {
-
-  browser.get('index.html');
-
-  it('should automatically redirect to /submitSales when location hash/fragment is empty', function() {
-    
-    browser.navigateTo('/');
-    expect(browser().location().path()).toBe('/events');
-
-        //expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
-
-/*
-  describe('view1', function() {
-
+describe('index page', function() {
     beforeEach(function() {
-      browser.get('index.html#/view1');
-    });
+        browser().navigateTo('/')
+    }); 
+    it ('redirect works', function() {
+        expect(browser().location().url()).toBe('/submitSales');
+    }); 
 
+    it ('has directive greeting', function() {
+        expect(element('nav').count()).toBe(1);
+    }); 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
+    it ('has partial greeting', function() {
+        expect(element('p.partial-hello').count()).toBe(0);
+    }); 
 
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });*/
+    it ('has model greeting', function() {
+        expect(element('p.model-hello').count()).toBe(0);
+    }); 
 });
